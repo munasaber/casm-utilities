0a1515210cfd72cb7b16da9b16b3a027a1e824e7
Muna Saber <munasaber@ucsb.edu>
2019-10-30 13:51:09 -0700

    Started FindHighSymmetryGroups

--- include/casmutils/stage.hpp
+++ include/casmutils/stage.hpp
@@ -4,8 +4,17 @@
 #include "casmutils/definitions.hpp"
 #include "casmutils/structure.hpp"
 
-namespace Simplicity
+namespace Rewrap
 {
-    void trivial_muna_function();
+    vector<SymOp> getfactor_group(struc my_Structure)
+    vector<SymOp> gettrace(vector<SymOp> myfactorgroup)
+    vector<double> geteigenvals(struc my_Structure, vector<SymOp> myfactorgroup)
+    bool test_for_identity(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    bool test_for_inversion(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    bool test_for_mirrorplane(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    bool test_for_rotation(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    vector<double> get_inversionpoint()
+    vector<vector<double>> get_mirrorplane()
+    vector<double> get_rotationaxis()
 }
 #endif
--- src/casmutils/stage.cxx
+++ src/casmutils/stage.cxx
@@ -7,12 +7,22 @@
 #include "casmutils/lattice.hpp"
 #include <casm/crystallography/Niggli.hh>
 
-namespace Simplicity
+
+#If you have a particular structure, find the inversion points, rotation axes, and mirrorplanes
+namespace Rewrap
 {
-    void trivial_muna_function()
-    {
-        std::cout<<"Nice haircut"<<std::endl;
-        return;
-    }
+    vector<SymOp> getfactor_group(struc my_Structure)
+    vector<SymOp> gettrace(vector<SymOp> myfactorgroup)
+    vector<double> geteigenvals(struc my_Structure, vector<SymOp> myfactorgroup)
+    bool test_for_identity(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    if  
+    bool test_for_inversion(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    testmatrix=[-1 0 0; 0 -1 0; 0 0 -1]
+    #SymOp.character() gives trace
+    bool test_for_mirrorplane(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    bool test_for_rotation(struc my_Structure, vector<SymOp> trace, vector<double> eigenvals)
+    vector<double> get_inversionpoint()
+    vector<vector<double>> get_mirrorplane()
+    vector<double> get_rotationaxis()
 
 } // namespace SuperBoxy
